0f757c320650e2be53a502c8c15130edc7d3b896
aksinghania <akshtsinghnia@gmail.com>
2024-07-28 21:23:31 +0530

    modified correct and incorrect output

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
-const typingText = document.querySelector(".typing-text");
+const typingText = document.querySelector(".typing-text p"),
+  inpField = document.querySelector(".wrapper .input-field");
+
+let charIndex = 0;
 
 function randomParagraph() {
   let randIndex = Math.floor(Math.random() * paragraphs.length);
-  paragraphs[randIndex].split("").forEach((span) => {
-    let spanTag = ` <span>${span}</span>`;
-    typingText.innerHTML = spanTag;
+
+  paragraphs[randIndex].split("").forEach((char) => {
+    let spanTag = `<span>${char}</span>`;
+    typingText.innerHTML += spanTag;
   });
 }
 
+function initTyping() {
+  const characters = typingText.querySelectorAll("span");
+
+  if (charIndex < characters.length) {
+    let typedChar = inpField.value.split("")[charIndex];
+
+    if (characters[charIndex].innerText === typedChar) {
+      characters[charIndex].classList.add("correct");
+    } else {
+      characters[charIndex].classList.add("incorrect");
+    }
+    charIndex++;
+  }
+}
+
+document.addEventListener("keydown", () => inpField.focus());
+typingText.addEventListener("click", () => inpField.focus());
+
 randomParagraph();
+inpField.addEventListener("input", initTyping);
