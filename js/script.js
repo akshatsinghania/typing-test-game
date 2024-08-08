90ef994b4d7c1f6e8681dcbaba6073fd0980e841
akshatsinghania <singhaniaakshat1@gmail.com>
2024-08-08 12:29:33 +0530

    added mistake function and added count to it

--- js/script.js
+++ js/script.js
@@ -1,34 +1,82 @@
 const typingText = document.querySelector(".typing-text p");
 const inpField = document.querySelector(".wrapper .input-field");
+const timeTag = document.querySelector(".time span b");
+const mistakeTag = document.querySelector(".mistake span");
+
+let timer,
+  maxTime = 60,
+  timeLeft = maxTime;
 
 let charIndex = 0;
+let mistakes = 0;
+let isTyping = false;
 
 function randomParagraph() {
   let randIndex = Math.floor(Math.random() * paragraphs.length);
-  typingText.innerHTML = ""; // Clear previous content
+  typingText.innerHTML = "";
   paragraphs[randIndex].split("").forEach((char) => {
     let spanTag = `<span>${char}</span>`;
     typingText.innerHTML += spanTag;
   });
-  charIndex = 0; // Reset charIndex for new paragraph
-  inpField.value = ""; // Clear input field
+  charIndex = 0;
+  inpField.value = "";
+  mistakes = 0;
+  timeLeft = maxTime;
+  mistakeTag.innerText = mistakes;
+  timeTag.innerText = timeLeft;
+  clearInterval(timer); 
+  isTyping = false; 
 }
 
 function initTyping() {
   const characters = typingText.querySelectorAll("span");
   let typedChar = inpField.value.split("")[charIndex];
-  if (charIndex < characters.length) {
-    if (characters[charIndex].innerText === typedChar) {
-      characters[charIndex].classList.add("correct");
-    } else {
-      characters[charIndex].classList.add("incorrect");
+
+  if (!isTyping) {
+    timer = setInterval(initTimer, 1000);
+    isTyping = true; 
+  }
+
+  if (typedChar == null) {
+    if (charIndex > 0) {
+      charIndex--;
+      if (characters[charIndex].classList.contains("incorrect")) {
+        mistakes--;
+        mistakeTag.innerText = mistakes;
+      }
+      characters[charIndex].classList.remove("correct", "incorrect");
     }
-    charIndex++;
+  } else {
+    if (charIndex < characters.length) {
+      if (characters[charIndex].innerText === typedChar) {
+        characters[charIndex].classList.add("correct");
+      } else {
+        mistakes++;
+        characters[charIndex].classList.add("incorrect");
+      }
+      charIndex++;
+    }
+
+    characters.forEach((span) => span.classList.remove("active"));
+    if (charIndex < characters.length) {
+      characters[charIndex].classList.add("active");
+    }
+
+    mistakeTag.innerText = mistakes;
   }
 }
 
 document.addEventListener("keydown", () => inpField.focus());
 typingText.addEventListener("click", () => inpField.focus());
+
+function initTimer() {
+  if (timeLeft > 0) {
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+  } else {
+    clearInterval(timer);
+  }
+}
 
 randomParagraph();
 inpField.addEventListener("input", initTyping);
